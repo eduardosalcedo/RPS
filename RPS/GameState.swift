50a328dc54037f89768e0db5c8a8443c4c1bf7fa
eduardo salcedo <salcedo.eduardo@gmail.com>
2023-02-23 09:32:13 -0500

    add game logic to app.

---
+++ RPS/GameState.swift
@@ -0,0 +1,15 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Eduardo Salcedo on 2/23/23.
+//
+
+import Foundation
+
+enum GameState {
+    case start
+    case win
+    case lose
+    case draw
+}
